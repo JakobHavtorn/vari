396d54fb9dc9e63d79f1b6d8f80782c2503837fb
Jakob Havtorn <jdh@corti.ai>
2019-12-20 10:23:21 +0000

    Update spirals sampling with rotation matrix

--- vari/datasets/spirals.py
+++ vari/datasets/spirals.py
@@ -3,31 +3,37 @@ import numpy as np
 from torch.utils.data import Dataset
 
 
-def make_data_spiral(n_samples=1000, shuffle=True, noise=.05, radius=1):
-    theta = np.sqrt(np.random.rand(n_samples)) * 2 * np.pi # np.linspace(0,2*pi,100)
+def make_data_spiral(n_samples=1000, shuffle=True, noise=.05, rotation=0):
+    rot_mat = np.array([[np.cos(rotation),-np.sin(rotation)],
+                        [np.sin(rotation), np.cos(rotation)]])
+    
+    #theta = np.sqrt(np.random.rand(n_samples)) * 2 * np.pi
+    theta = np.linspace(0, 2 * np.pi, n_samples)
 
-    r_a = 2*theta + np.pi
-    data_a = np.array([np.cos(theta)*r_a, np.sin(theta)*r_a]).T
+    r_a = -theta - np.pi
+    data_a = np.array([np.cos(theta) * r_a, np.sin(theta) * r_a]).T
     x_a = data_a + noise * np.random.randn(n_samples, 2)
+    x_a = np.dot(x_a, rot_mat)
 
-    r_b = -2*theta - np.pi
-    data_b = np.array([np.cos(theta)*r_b, np.sin(theta)*r_b]).T
+    r_b = theta + np.pi
+    data_b = np.array([np.cos(theta) * r_b, np.sin(theta) * r_b]).T
     x_b = data_b + noise * np.random.randn(n_samples, 2)
-
+    x_b = np.dot(x_b, rot_mat)
+    
     res_a = np.append(x_a, np.zeros((n_samples, 1)), axis=1)
     res_b = np.append(x_b, np.ones((n_samples, 1)), axis=1)
 
     res = np.append(res_a, res_b, axis=0)
     np.random.shuffle(res)
 
-    values = radius * res[:, :2] / np.abs(res[:, :2]).max()
+    values = res[:, :2]
     labels = np.identity(res[:, 2].astype(int).max() + 1)[res[:, 2].astype(int)]
     return values.astype(np.float32), labels.astype(np.float32)
 
 
 class Spirals(Dataset):
-    def __init__(self, n_samples=1000, noise=0.05, radius=1):
-        examples, labels = make_data_spiral(n_samples=n_samples, noise=noise, radius=radius)
+    def __init__(self, n_samples=1000, noise=0.05, rotation=0):
+        examples, labels = make_data_spiral(n_samples=n_samples, noise=noise, rotation=rotation)
         self.examples = examples
         self.labels = labels
         
