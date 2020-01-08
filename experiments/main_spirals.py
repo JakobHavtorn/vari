8b3b58157487568ccfc8599bedffe42173d1d580
Jakob Havtorn <jdh@corti.ai>
2020-01-08 18:24:34 +0000

    importance weighting debugging

--- experiments/main_spirals.py
+++ experiments/main_spirals.py
@@ -3,6 +3,7 @@ import torch
 import pickle
 
 from torch.utils.data import DataLoader
+from pprint import pprint
 
 from model_utils.experiment import Experiment
 
@@ -20,29 +21,35 @@ ex = Experiment(name='OOD VAE')
 @ex.config
 def default_configuration():
     tag = 'ood-detection'
-    n_epochs = 1000
+    n_epochs = 300
     batch_size = 32
     importance_samples = 10
     learning_rate = 3e-4
-    dataset = 'spirals'  # 'moons'
-    vae_type = 'DeepVariationalAutoencoder'  # LadderVariationalAutoencoder, AuxilliaryVariationalAutoencoder, LadderVariationalAutoencoder
-    vae_kwargs = dict(
-        x_dim=2,
-        z_dim=[2, 2],
-        h_dim=[64, 64, 64]
-    )
-    # vae_type = 'VariationalAutoencoder'  # LadderVariationalAutoencoder, AuxilliaryVariationalAutoencoder, LadderVariationalAutoencoder
+    dataset = 'moons'  # 'moons'
+    # vae_type = 'DeepVariationalAutoencoder'  # LadderVariationalAutoencoder, AuxilliaryVariationalAutoencoder, LadderVariationalAutoencoder
     # vae_kwargs = dict(
     #     x_dim=2,
-    #     z_dim=2,
-    #     h_dim=[64, 64, 64]
+    #     z_dim=[2, 2],
+    #     h_dim=[64, 64]
     # )
+    vae_type = 'VariationalAutoencoder'  # LadderVariationalAutoencoder, AuxilliaryVariationalAutoencoder, LadderVariationalAutoencoder
+    vae_kwargs = dict(
+        x_dim=2,
+        z_dim=2,
+        h_dim=[64, 64]
+    )
     warmup_epochs = 0
     device = get_device()
+    seed = 0
 
 
 @ex.automain
-def run(device, vae_type, vae_kwargs, dataset, n_epochs, batch_size, learning_rate, importance_samples, warmup_epochs):
+def run(device, vae_type, vae_kwargs, dataset, n_epochs, batch_size, learning_rate, importance_samples, warmup_epochs,
+        seed):
+    pprint(ex.current_run.config)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    
     if dataset == 'spirals':
         train_dataset = Spirals(n_samples=1000, noise=0.05, rotation=0)
     elif dataset == 'moons':
@@ -73,15 +80,26 @@ def run(device, vae_type, vae_kwargs, dataset, n_epochs, batch_size, learning_ra
             for b, (x, _) in enumerate(train_loader):
                 x = x.to(device)
 
-                x = x.repeat(1, importance_samples).view(-1, x.shape[1])
+                optimizer.zero_grad()
                 
-                px, px_mu, px_sigma = model(x)
+                # if epoch == 200 and b == 0:
+                #     import IPython
+                #     IPython.embed()
+
+                x_iw = x.repeat(1, importance_samples).view(-1, x.shape[1])
+                
+                px, px_mu, px_sigma = model(x_iw)
                 kl_divergence = model.kl_divergence
 
-                likelihood = log_gaussian(x, px_mu, px_sigma)
-                elbo = likelihood - beta * kl_divergence
+                likelihood = log_gaussian(x_iw, px_mu, px_sigma)
+                elbo = likelihood #- beta * kl_divergence
 
                 # Importance sampling
+                # NOTE: Training is slow compared to TF version and ELBO values are muddy
+                # NOTE: Results are identical with IW removed and IW=1
+                # NOTE: Training without KL loss works but is slower and has lower final ELBO than TF
+                # NOTE: 
+                # TODO Are we mixing the order of the importance samples??
                 elbo = log_sum_exp(elbo.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  # (B, 1, 1)
                 kl_divergence = log_sum_exp(kl_divergence.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  # (B, 1, 1)
                 likelihood = log_sum_exp(likelihood.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  # (B, 1, 1)
@@ -90,7 +108,6 @@ def run(device, vae_type, vae_kwargs, dataset, n_epochs, batch_size, learning_ra
 
                 loss.backward()
                 optimizer.step()
-                optimizer.zero_grad()
 
                 total_elbo += elbo.mean().item()
                 total_log_px += likelihood.mean().item()
@@ -101,9 +118,34 @@ def run(device, vae_type, vae_kwargs, dataset, n_epochs, batch_size, learning_ra
                 ex.log_scalar(f'(batch) log p(x|z)', total_log_px / (b + 1), step=i_update)
                 ex.log_scalar(f'(batch) KL(q(z|x)||p(z))', total_kl / (b + 1), step=i_update)
                 ex.log_scalar(f'(batch) ß * KL(q(z|x)||p(z))', beta * total_kl / (b + 1), step=i_update)
+                
 
-            # import IPython
-            # IPython.embed()
+                # elbo_test = torch.Tensor([1,1,1,2,3,4,3,3,4])
+                # log_sum_exp(elbo_test.view(-1, 3, 1), axis=1, sum_op=torch.mean).view(-1, 1)
+                # elbo_test = torch.Tensor([1,2,3,4,5])
+                # log_sum_exp(elbo_test.view(-1, 1, 1), axis=1, sum_op=torch.mean).view(-1, 1)
+                
+                # USE IW
+                # torch.manual_seed(0)
+                # importance_samples = 1
+                # x_iw = x.repeat(1, importance_samples).view(-1, x.shape[1])
+                # px, px_mu, px_sigma = model(x_iw)
+                # kl_divergence = model.kl_divergence
+                # likelihood = log_gaussian(x_iw, px_mu, px_sigma)
+                # elbo = likelihood - beta * kl_divergence
+                # print(elbo.view(-1, importance_samples, 1))
+                # elbo = log_sum_exp(elbo.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  # (B, 1, 1)
+                # print(elbo)
+                
+                # # # DON'T USE IW
+                # torch.manual_seed(0)
+                # px, px_mu, px_sigma = model(x)
+                # kl_divergence = model.kl_divergence
+                # likelihood = log_gaussian(x, px_mu, px_sigma)
+                # elbo = likelihood - beta * kl_divergence
+                # print(elbo.view(-1, 1, 1))
+                # elbo = log_sum_exp(elbo.view(-1, 1, 1), axis=1, sum_op=torch.mean).view(-1, 1)  # (B, 1, 1)
+                # print(elbo)
                 
             total_elbo = total_elbo / len(train_loader)
             total_log_px = total_log_px / len(train_loader)
