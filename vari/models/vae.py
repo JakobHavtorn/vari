2804c19f55d200aafaea8d517a943d5c9d26f639
Jakob Havtorn <jdh@corti.ai>
2020-01-09 16:10:53 +0000

    Try to implement more general LVAE

--- vari/models/vae.py
+++ vari/models/vae.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.nn import init
 
-from vari.layers import GaussianSample, GaussianMerge, GumbelSoftmax
+from vari.layers import IdentityLayer, GaussianSample, GaussianMerge, GumbelSoftmax
 from vari.inference import log_gaussian, log_standard_gaussian
 from vari.inference.divergence import kld_gaussian_gaussian, kld_gaussian_gaussian_analytical
 
 
 class Perceptron(nn.Module):
     def __init__(self, dims, activation_fn=F.relu, output_activation=None):
-        super(Perceptron, self).__init__()
+        super().__init__()
         self.dims = dims
         self.activation_fn = activation_fn
         self.output_activation = output_activation
@@ -41,7 +41,7 @@ class DenseSequentialCoder(nn.Module):
         [input_dim, [hidden_dims], latent_dim].
     """
     def __init__(self, x_dim, z_dim, h_dim, activation=nn.Tanh, sample_layer=GaussianSample):
-        super(DenseSequentialCoder, self).__init__()
+        super().__init__()
 
         self.x_dim = x_dim
         self.z_dim = z_dim
@@ -70,7 +70,7 @@ class VariationalAutoencoder(nn.Module):
     :param dims: x, z and hidden dimensions of the networks
     """
     def __init__(self, x_dim, z_dim, h_dim):
-        super(VariationalAutoencoder, self).__init__()
+        super().__init__()
 
         self.x_dim = x_dim
         self.z_dim = z_dim
@@ -277,31 +277,30 @@ class AuxilliaryVariationalAutoencoder(nn.Module):
 
 
 class LadderEncoder(nn.Module):
-    """
-    The ladder encoder differs from the standard encoder
-    by using batch-normalization and LReLU activation.
-    Additionally, it also returns the transformation x.
-    :param dims: dimensions [input_dim, [hidden_dims], [latent_dims]].
-    """
-    def __init__(self, dims, activation=nn.Tanh):
+    def __init__(self, dims):
+        """
+        The ladder encoder differs from the standard encoder
+        by using batch-normalization and LReLU activation.
+        Additionally, it also returns the transformation x.
+        :param dims: dimensions [input_dim, [hidden_dims], [latent_dims]].
+        """
         super(LadderEncoder, self).__init__()
         [x_dim, h_dim, self.z_dim] = dims
         self.in_features = x_dim
         self.out_features = h_dim
 
-        self.activation = activation()
         self.linear = nn.Linear(x_dim, h_dim)
         self.batchnorm = nn.BatchNorm1d(h_dim)
         self.sample = GaussianSample(h_dim, self.z_dim)
 
     def forward(self, x):
         x = self.linear(x)
-        x = self.activation(self.batchnorm(x))
+        x = F.tanh(self.batchnorm(x))
         return x, self.sample(x)
 
 
 class LadderDecoder(nn.Module):
-    def __init__(self, dims, activation=nn.Tanh):
+    def __init__(self, dims):
         """
         The ladder dencoder differs from the standard encoder
         by using batch-normalization and LReLU activation.
@@ -316,24 +315,22 @@ class LadderDecoder(nn.Module):
 
         self.linear1 = nn.Linear(x_dim, h_dim)
         self.batchnorm1 = nn.BatchNorm1d(h_dim)
-        self.activation1 = activation()
         self.merge = GaussianMerge(h_dim, self.z_dim)
 
         self.linear2 = nn.Linear(x_dim, h_dim)
         self.batchnorm2 = nn.BatchNorm1d(h_dim)
-        self.activation2 = activation()
         self.sample = GaussianSample(h_dim, self.z_dim)
 
     def forward(self, x, l_mu=None, l_log_var=None):
         if l_mu is not None:
             # Sample from this encoder layer and merge
             z = self.linear1(x)
-            z = self.activation1(self.batchnorm1(z))
+            z = F.tanh(self.batchnorm1(z))
             q_z, q_mu, q_log_var = self.merge(z, l_mu, l_log_var)
 
         # Sample from the decoder and send forward
         z = self.linear2(x)
-        z = self.activation2(self.batchnorm2(z))
+        z = F.tanh(self.batchnorm2(z))
         z, p_mu, p_log_var = self.sample(z)
 
         if l_mu is None:
@@ -351,13 +348,14 @@ class LadderVariationalAutoencoder(nn.Module):
         :param dims: x, z and hidden dimensions of the networks
         """
         super().__init__()
-        dims = [x_dim, *h_dim]
-        encoder_layers = [LadderEncoder([dims[i - 1], dims[i], z_dim[i - 1]]) for i in range(1, len(dims))]
+
+        neurons = [x_dim, *h_dim]
+        encoder_layers = [LadderEncoder([neurons[i - 1], neurons[i], z_dim[i - 1]]) for i in range(1, len(neurons))]
         decoder_layers = [LadderDecoder([z_dim[i - 1], h_dim[i - 1], z_dim[i]]) for i in range(1, len(h_dim))][::-1]
 
         self.encoder = nn.ModuleList(encoder_layers)
         self.decoder = nn.ModuleList(decoder_layers)
-        self.reconstruction = DenseSequentialCoder(x_dim=x_dim, z_dim=z_dim[0], h_dim=h_dim)
+        self.reconstruction = DenseSequentialCoder(x_dim=z_dim[0], z_dim=x_dim, h_dim=h_dim)
         self.initialize()
 
     def initialize(self):
@@ -367,41 +365,193 @@ class LadderVariationalAutoencoder(nn.Module):
                 init.xavier_normal_(m.weight, gain=gain)
                 if m.bias is not None:
                     m.bias.data.zero_()
-                    
-    def encode(self, x):
+
+    def forward(self, x):
+        # Gather latent representation
+        # from encoders along with final z.
         latents = []
         for encoder in self.encoder:
-            x, (z, z_mu, z_log_var) = encoder(x)
-            latents.append((z, z_mu, z_log_var))
-        return latents
+            x, (z, mu, log_var) = encoder(x)
+            latents.append((mu, log_var))
 
-    def decode(self, latents):
+        latents = list(reversed(latents))
+
         self.kl_divergence = 0
-        for i, decoder in enumerate([None, *self.decoder]):
-            _, l_mu, l_log_var = latents[i]
+        for i, decoder in enumerate([-1, *self.decoder]):
+            # If at top, encoder == decoder,
+            # use prior for KL.
+            l_mu, l_log_var = latents[i]
             if i == 0:
-                # If at top, encoder == decoder, use prior for KL.
-                z = latents[i][0]
                 self.kl_divergence += kld_gaussian_gaussian(z, (l_mu, l_log_var))
+
+            # Perform downword merge of information.
             else:
-                # Perform downward merge of information.
                 z, kl = decoder(z, l_mu, l_log_var)
                 self.kl_divergence += kld_gaussian_gaussian(*kl)
 
-        p_x, p_x_mu, p_x_log_var = self.reconstruction(z)
-        return p_x, p_x_mu, p_x_log_var
-
-    def forward(self, x):
-        # Gather latent representation from encoders along with final z.
-        latents = self.encode(x)
-        latents = list(reversed(latents))
-        p_x, p_x_mu, p_x_log_var = self.decode(latents)
-        return p_x, p_x_mu, p_x_log_var
+        x_mu = self.reconstruction(z)
+        return x_mu
 
     def sample(self, z):
         for decoder in self.decoder:
             z = decoder(z)
         return self.reconstruction(z)
+
+
+# NOTE First go at implementing variable number of hidden layers between stochastic variables
+# class LadderEncoder(nn.Module):
+#     """
+#     Inference network
+#     Attempts to infer the probability distribution
+#     p(z|x) from the data by fitting a variational
+#     distribution q_φ(z|x). Returns the two parameters
+#     of the distribution (µ, log σ²).
+#     :param dims: dimensions of the networks
+#         given by the number of neurons on the form
+#         [input_dim, [hidden_dims], latent_dim].
+#     """
+#     def __init__(self, x_dim, z_dim, h_dim, activation=nn.Tanh, sample_layer=GaussianSample):
+#         super().__init__()
+#         # TODO Make batchnormalization optional
+#         # TODO Collapse this encoder into the DenseSequentialCoder
+
+#         self.x_dim = x_dim
+#         self.z_dim = z_dim
+#         self.h_dim = [h_dim] if isinstance(h_dim, int) else h_dim
+        
+#         dims = [x_dim, *self.h_dim]
+#         linear_layers = [nn.Linear(dims[i-1], dims[i]) for i in range(1, len(dims))]
+#         activations = [activation() for _ in range(len(linear_layers))]
+#         batchnorms = [nn.BatchNorm1d(dims[i]) for i in range(0, len(dims))]
+
+#         self.hidden = nn.ModuleList(linear_layers)
+#         self.activations = nn.ModuleList(activations)
+#         self.batchnorms = nn.ModuleList(batchnorms)
+#         self.sample = sample_layer(self.h_dim[-1], z_dim)
+
+#     def forward(self, x):
+#         for batchnorm, layer, activation in zip(self.batchnorms, self.hidden, self.activations):
+#             x = activation(layer(batchnorm(x)))
+#         return self.sample(x)
+
+
+# class LadderDecoder(nn.Module):
+#     def __init__(self, dims, activation=nn.Tanh):
+#         """
+#         The ladder dencoder differs from the standard encoder
+#         by using batch-normalization and LReLU activation.
+#         Additionally, it also returns the transformation x.
+#         :param dims: dimensions of the networks
+#             given by the number of neurons on the form
+#             [latent_dim, [hidden_dims], input_dim].
+#         """
+#         super(LadderDecoder, self).__init__()
+#         # self.x_dim = x_dim
+#         # self.z_dim = z_dim
+#         # self.h_dim = h_dim
+#         [z_dim, h_dim, x_dim] = dims
+#         self.z_dim = z_dim
+        
+#         import IPython
+#         IPython.embed()
+
+#         self.coder1 = LadderEncoder(x_dim=x_dim, z_dim=z_dim, h_dim=h_dim, sample_layer=IdentityLayer)
+#         # self.linear1 = nn.Linear(x_dim, h_dim)
+#         # self.batchnorm1 = nn.BatchNorm1d(h_dim)
+#         # self.activation1 = activation()
+#         self.merge = GaussianMerge(h_dim, self.z_dim)
+
+#         self.coder2 = LadderEncoder(x_dim=x_dim, z_dim=z_dim, h_dim=h_dim, sample_layer=IdentityLayer)
+#         # self.linear2 = nn.Linear(x_dim, h_dim)
+#         # self.batchnorm2 = nn.BatchNorm1d(h_dim)
+#         # self.activation2 = activation()
+#         # self.sample = GaussianSample(h_dim, self.z_dim)
+
+#     def forward(self, x, l_mu=None, l_log_var=None):
+#         if l_mu is not None:
+#             # Sample from this encoder layer and merge
+#             # z = self.linear1(x)
+#             # z = self.activation1(self.batchnorm1(z))
+#             z = self.coder1(x)
+#             q_z, q_mu, q_log_var = self.merge(z, l_mu, l_log_var)
+
+#         # Sample from the decoder and send forward
+#         # z = self.linear2(x)
+#         # z = self.activation2(self.batchnorm2(z))
+#         z = self.coder2(x)
+#         z, p_mu, p_log_var = self.sample(z)
+
+#         if l_mu is None:
+#             return z
+
+#         return z, (q_z, (q_mu, q_log_var), (p_mu, p_log_var))
+
+
+# class LadderVariationalAutoencoder(nn.Module):
+#     def __init__(self, x_dim, z_dim, h_dim):        
+#         """
+#         Ladder Variational Autoencoder as described by
+#         [Sønderby 2016]. Adds several stochastic
+#         layers to improve the log-likelihood estimate.
+        
+#         Args:
+#             x_dim (int): Dimensionality of input
+#             z_dim (list of int): Dimensionality of the latent stochastic variables
+#             h_dim (list of list of int): Dimensionality of each of the hidden layers for each of the latent variables
+#         """
+#         super().__init__()
+#         dims = [x_dim, *z_dim]
+
+#         encoder_layers = [LadderEncoder(x_dim=dims[i - 1], h_dim=h_dim[i - 1], z_dim=dims[i]) for i in range(1, len(dims))]
+#         decoder_layers = [LadderDecoder([dims[i - 1], h_dim[i - 1], dims[i]]) for i in range(1, len(h_dim))][::-1]
+
+#         self.encoder = nn.ModuleList(encoder_layers)
+#         self.decoder = nn.ModuleList(decoder_layers)
+#         self.reconstruction = DenseSequentialCoder(x_dim=x_dim, z_dim=z_dim[0], h_dim=h_dim)
+#         self.initialize()
+
+#     def initialize(self):
+#         gain = torch.nn.init.calculate_gain('tanh', param=None)
+#         for m in self.modules():
+#             if isinstance(m, nn.Linear):
+#                 init.xavier_normal_(m.weight, gain=gain)
+#                 if m.bias is not None:
+#                     m.bias.data.zero_()
+                    
+#     def encode(self, x):
+#         latents = []
+#         for encoder in self.encoder:
+#             x, (z, z_mu, z_log_var) = encoder(x)
+#             latents.append((z, z_mu, z_log_var))
+#         return latents
+
+#     def decode(self, latents):
+#         self.kl_divergence = 0
+#         for i, decoder in enumerate([None, *self.decoder]):
+#             _, l_mu, l_log_var = latents[i]
+#             if i == 0:
+#                 # If at top, encoder == decoder, use prior for KL.
+#                 z = latents[i][0]
+#                 self.kl_divergence += kld_gaussian_gaussian(z, (l_mu, l_log_var))
+#             else:
+#                 # Perform downward merge of information.
+#                 z, kl = decoder(z, l_mu, l_log_var)
+#                 self.kl_divergence += kld_gaussian_gaussian(*kl)
+
+#         p_x, p_x_mu, p_x_log_var = self.reconstruction(z)
+#         return p_x, p_x_mu, p_x_log_var
+
+#     def forward(self, x):
+#         # Gather latent representation from encoders along with final z.
+#         latents = self.encode(x)
+#         latents = list(reversed(latents))
+#         p_x, p_x_mu, p_x_log_var = self.decode(latents)
+#         return p_x, p_x_mu, p_x_log_var
+
+#     def sample(self, z):
+#         for decoder in self.decoder:
+#             z = decoder(z)
+#         return self.reconstruction(z)
     
 
 class GumbelAutoencoder(nn.Module):
