FashionMNIST and HVAE KL explosion fix

vari/datasets/mnist.py
@@ -3,14 +3,17 @@ import urllib
 
 import numpy as np
 
-from torch.utils.data import Dataset
 import torchvision.datasets
+
+from torch.utils.data import Dataset
 
 
 def onehot_encode(array, max_label=None):
     max_label = np.max(array) + 1 if max_label is None else max_label
     return np.eye(max_label)[array]
 
+
+# TODO Abstract base class for MNIST-like datasets
 
 
 class MNISTInterpolate(Dataset):
@@ -48,21 +51,21 @@ class MNISTInterpolate(Dataset):
         self.exclude_labels = [] if exclude_labels is None else exclude_labels
 
         np.random.seed(seed)
-        mnist_train = torchvision.datasets.MNIST(root=root, train=True, transform=transform,
+        data_train = torchvision.datasets.MNIST(root=root, train=True, transform=transform,
                                                  target_transform=target_transform, download=download)
-        mnist_test = torchvision.datasets.MNIST(root=root, train=False, transform=transform,
+        data_test = torchvision.datasets.MNIST(root=root, train=False, transform=transform,
                                                 target_transform=target_transform, download=download)
 
         if split != 'join':
             if split == 'train':
-                self.examples = np.array(mnist_train.data)
-                self.labels = onehot_encode(np.array(mnist_train.targets))
+                self.examples = np.array(data_train.data)
+                self.labels = onehot_encode(np.array(data_train.targets))
             else:
-                self.examples = np.array(mnist_test.data)
-                self.labels = onehot_encode(np.array(mnist_test.targets))
+                self.examples = np.array(data_test.data)
+                self.labels = onehot_encode(np.array(data_test.targets))
         else:
-            self.examples = np.concatenate([mnist_train.data, mnist_test.data])
-            self.labels = onehot_encode(np.concatenate([mnist_train.targets, mnist_test.targets]))
+            self.examples = np.concatenate([data_train.data, data_test.data])
+            self.labels = onehot_encode(np.concatenate([data_train.targets, data_test.targets]))
 
         if preprocess == 'static':
             self.examples = self.preprocess(self.examples, seed=seed)
@@ -124,11 +127,11 @@ class MNISTInterpolate(Dataset):
         return self.examples.shape[0]
     
     def __repr__(self):
-        s = f'MNISTContinuous('
+        s = f'MNISTInterpolate('
         s += ', '.join([f'{attr}={getattr(self, attr)}' for attr in ['split', 'exclude_labels', 'gamma']])
         return s + ')'
     
-        
+
 class MNISTBinarized(Dataset):
     """MNIST dataset including filtering and concationation of train and test sets.
     
@@ -162,24 +165,24 @@ class MNISTBinarized(Dataset):
         self.exclude_labels = [] if exclude_labels is None else exclude_labels
 
         np.random.seed(seed)
-        mnist_train = torchvision.datasets.MNIST(root=root, train=True, transform=transform,
+        data_train = torchvision.datasets.MNIST(root=root, train=True, transform=transform,
                                                  target_transform=target_transform, download=download)
-        mnist_test = torchvision.datasets.MNIST(root=root, train=False, transform=transform,
+        data_test = torchvision.datasets.MNIST(root=root, train=False, transform=transform,
                                                 target_transform=target_transform, download=download)
 
         if split != 'join':
             if split == 'train':
-                self.examples = np.array(mnist_train.data)
-                self.labels = onehot_encode(np.array(mnist_train.targets))
+                self.examples = np.array(data_train.data)
+                self.labels = onehot_encode(np.array(data_train.targets))
             else:
-                self.examples = np.array(mnist_test.data)
-                self.labels = onehot_encode(np.array(mnist_test.targets))
+                self.examples = np.array(data_test.data)
+                self.labels = onehot_encode(np.array(data_test.targets))
         else:
-            self.examples = np.concatenate([mnist_train.data, mnist_test.data])
-            self.labels = onehot_encode(np.concatenate([mnist_train.targets, mnist_test.targets]))
+            self.examples = np.concatenate([data_train.data, data_test.data])
+            self.labels = onehot_encode(np.concatenate([data_train.targets, data_test.targets]))
 
         if preprocess == 'static':
-            self.examples = self.preprocess(self.examples, seed=seed)
+            self.examples = self.preprocess(self.examples)
 
         self.examples, self.labels = self.filter(self.examples, self.labels, exclude_labels)
 
@@ -243,9 +246,9 @@ class MNISTBinarizedLarochelle(Dataset):
         :return: The train, test and validation set.
         """
         split_urls = {
-            "train": "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_mnist_train.amat",
-            "valid": "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_mnist_valid.amat",
-            "test": "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_mnist_test.amat"
+            "train": "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_data_train.amat",
+            "valid": "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_data_valid.amat",
+            "test": "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_data_test.amat"
         }
         if not os.path.exists(os.path.join(root, 'MNISTBinarized')):
             os.makedirs(os.path.join(root, 'MNISTBinarized'))
@@ -268,5 +271,172 @@ class MNISTBinarizedLarochelle(Dataset):
 
     def __repr__(self):
         s = f'MNISTBinarizedLarochelle('
+        s += ', '.join([f'{attr}={getattr(self, attr)}' for attr in ['split', 'exclude_labels']])
+        return s + ')'
+
+
+class FashionMNISTInterpolate(Dataset):
+    """FashionMNIST dataset including filtering and concationation of train and test sets.
+    
+    Serves values in [0, 1] with gamma=0 returning the original dataset, gamma=-0.5 binarizing the dataset statically
+    and gamma=0.5 completely degrading the dataset to have all values set to 0.5.
+    
+    The preprocessing, which consists of the addition of uniform noise to the raw pixel values and then interpolable
+    binarization through setting gamma, can be done once ahead of training `preprocess=='static'` or done anew for each
+    example while training, `preprocess=='dynamic'`.
+    
+    Args:
+        split (str): Whether to serve the 'train' ir 'test' sets or 'join' the two to a single set.
+        exclude_labels (list): List of integer labels to exclude from the dataset.
+        gamma (float): Value in range [-0.5, 0.5] which interpolates between binarization (-0.5) and degrading (0.5)
+        preprocess (bool): If True, performs preprocessing ones before training. Else, preprocessing is done dynamically
+        
+    NOTE If preprocess is True, the noise added to the dataset at different values of `split` is NOT the same pe
+         example. I.e. examples from the training set are modified differently when served with `split=='train'` and 
+         `split=='join'`.
+    """
+
+    def __init__(self, split='train', exclude_labels=None, gamma=0.0, preprocess='dynamic', seed=0, root='torch_data/',
+                 transform=None, target_transform=None, download=True):
+        super().__init__()
+        
+        assert preprocess in ['dynamic', 'static']
+        assert split in ['train', 'test', 'join']
+
+        self.gamma = gamma
+        self.split = split
+        self.seed = seed
+        self.dynamic_preprocessing = preprocess == 'dynamic'
+        self.exclude_labels = [] if exclude_labels is None else exclude_labels
+
+        np.random.seed(seed)
+        data_train = torchvision.datasets.FashionMNIST(root=root, train=True, transform=transform,
+                                                        target_transform=target_transform, download=download)
+        data_test = torchvision.datasets.FashionMNIST(root=root, train=False, transform=transform,
+                                                       target_transform=target_transform, download=download)
+
+        if split != 'join':
+            if split == 'train':
+                self.examples = np.array(data_train.data)
+                self.labels = onehot_encode(np.array(data_train.targets))
+            else:
+                self.examples = np.array(data_test.data)
+                self.labels = onehot_encode(np.array(data_test.targets))
+        else:
+            self.examples = np.concatenate([data_train.data, data_test.data])
+            self.labels = onehot_encode(np.concatenate([data_train.targets, data_test.targets]))
+
+        if preprocess == 'static':
+            self.examples = self.preprocess(self.examples, seed=seed)
+            self.examples = self.warp_pixels(self.examples, gamma)
+
+        self.examples, self.labels = self.filter(self.examples, self.labels, exclude_labels)
+        
+    def __getitem__(self, idx):
+        return MNISTInterpolate.__getitem__(idx)
+
+    @staticmethod
+    def filter(examples, labels, exclude_labels):
+        return MNISTInterpolate.filter(examples, labels, exclude_labels)
+
+    @staticmethod
+    def preprocess(examples):
+        return MNISTInterpolate.preprocess(examples)
+
+    @staticmethod
+    def warp_pixels(examples, gamma):
+        return MNISTInterpolate.warp_pixels(examples, gamma)
+
+    def __len__(self):
+        return MNISTInterpolate.__len__()
+    
+    def __repr__(self):
+        s = f'FashionMNISTInterpolate('
+        s += ', '.join([f'{attr}={getattr(self, attr)}' for attr in ['split', 'exclude_labels', 'gamma']])
+        return s + ')'
+
+
+class FashionMNISTBinarized(Dataset):
+    """FashionMNIST dataset including filtering and concationation of train and test sets.
+    
+    Serves binarized values in {0, 1}.
+    
+    The preprocessing, which consists of binarization using the normalized pixel values [0, 1] as binomial
+    probabilities, can either be done once ahead of training `preprocess=='static'` or done anew for each
+    example while training, `preprocess=='dynamic'`. Setting the `seed` will ensure reproducibility in either case.
+    
+    Args:
+        split (str): Whether to serve the 'train' ir 'test' sets or 'join' the two to a single set.
+        exclude_labels (list): List of integer labels to exclude from the dataset.
+        gamma (float): Value in range [-0.5, 0.5] which interpolates between binarization (-0.5) and degrading (0.5)
+        preprocess (bool): If True, performs preprocessing ones before training. Else, preprocessing is done dynamically
+        
+    NOTE If preprocess is True, the noise added to the dataset at different values of `split` is NOT the same pe
+         example. I.e. examples from the training set are modified differently when served with `split=='train'` and 
+         `split=='join'`.
+    """
+
+    def __init__(self, split='train', exclude_labels=None, preprocess='dynamic', seed=0, root='torch_data/',
+                 transform=None, target_transform=None, download=True):
+        super().__init__()
+        
+        assert preprocess in ['dynamic', 'static']
+        assert split in ['train', 'test', 'join']
+        
+        self.split = split
+        self.seed = seed
+        self.dynamic_preprocessing = preprocess == 'dynamic'
+        self.exclude_labels = [] if exclude_labels is None else exclude_labels
+
+        np.random.seed(seed)
+        data_train = torchvision.datasets.FashionMNIST(root=root, train=True, transform=transform,
+                                                 target_transform=target_transform, download=download)
+        data_test = torchvision.datasets.FashionMNIST(root=root, train=False, transform=transform,
+                                                target_transform=target_transform, download=download)
+
+        if split != 'join':
+            if split == 'train':
+                self.examples = np.array(data_train.data)
+                self.labels = onehot_encode(np.array(data_train.targets))
+            else:
+                self.examples = np.array(data_test.data)
+                self.labels = onehot_encode(np.array(data_test.targets))
+        else:
+            self.examples = np.concatenate([data_train.data, data_test.data])
+            self.labels = onehot_encode(np.concatenate([data_train.targets, data_test.targets]))
+
+        if preprocess == 'static':
+            self.examples = self.preprocess(self.examples)
+
+        self.examples, self.labels = self.filter(self.examples, self.labels, exclude_labels)
+
+    def __getitem__(self, idx):
+        example = self.examples[idx].astype(np.float32)
+        if self.dynamic_preprocessing:
+            example = self.preprocess(example)
+        return example.astype(np.float32), self.labels[idx].astype(np.float32)
+
+    @staticmethod
+    def filter(examples, labels, exclude_labels):
+        """Filter the dataset and return examples and labels without the excluded labels"""
+        return MNISTInterpolate.filter(examples, labels, exclude_labels)
+
+    @staticmethod
+    def preprocess(examples):
+        """Standard preprocessing of MNIST.
+
+        Adds uniform [0, 1] noise to the integer pixel values between 0 and 255 and then divides by 256.
+        This results in values in [0, 1].
+        """
+        examples = examples.astype(np.float64)
+        examples /= 255  # /= examples.max()
+        examples = np.random.binomial(1, examples)  # Binary sampling with normalized pixel values as probabilities
+        return examples
+
+    def __len__(self):
+        return self.examples.shape[0]
+
+    def __repr__(self):
+        s = f'FashionMNISTBinarized('
         s += ', '.join([f'{attr}={getattr(self, attr)}' for attr in ['split', 'exclude_labels']])
         return s + ')'

vari/layers/layers.py
@@ -64,7 +64,7 @@ class GaussianSample(Stochastic):
         scale = [nn.Linear(in_features, out_features)]
         if scale_as == 'std':
             scale.append(nn.Softplus())
-            scale.append(Lambda(lambda x: x + 0.1))  
+            scale.append(Lambda(lambda x: x + 1e-8))  
         self.scale = nn.Sequential(*scale)
 
     def forward(self, x):