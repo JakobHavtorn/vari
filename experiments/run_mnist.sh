7284c2ca9a98278685dd91bfc2c87f1405ff8c81
Jakob Havtorn <jdh@corti.ai>
2020-01-17 15:49:42 +0000

    Update run scripts

--- experiments/run_mnist.sh
+++ experiments/run_mnist.sh
@@ -20,9 +20,18 @@ env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD Fashio
 
 
 # Excluded labels FashionMNIST and MNIST datasets, VAE, 2-layer VAE and AVAE models
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized VAE EXCL=[4] [2] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'excluded_labels=[4]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=VariationalAutoencoder'
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized VAE [2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'excluded_labels=[4]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=VariationalAutoencoder'
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized HVAE [5, 2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'excluded_labels=[4]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=HierarchicalVariationalAutoencoder'
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized HVAE [5, 2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'excluded_labels=[4]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=HierarchicalVariationalAutoencoder'
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized AVAE [2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'excluded_labels=[4]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=AuxilliaryVariationalAutoencoder'
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized AVAE [2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'excluded_labels=[4]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=AuxilliaryVariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized VAE [2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[4]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=VariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized HVAE [5, 2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[4]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=HierarchicalVariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized AVAE [2] EXCL=[4] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[4]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=AuxilliaryVariationalAutoencoder'
+
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized VAE [2] EXCL=[0] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[0]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=VariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized HVAE [5, 2] EXCL=[0] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[0]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=HierarchicalVariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized AVAE [2] EXCL=[0] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[0]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=AuxilliaryVariationalAutoencoder'
+
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized VAE [2] EXCL=[8] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=VariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized HVAE [5, 2] EXCL=[8] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[8]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=HierarchicalVariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD MNISTBinarized AVAE [2] EXCL=[8] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[8]' 'warmup_epochs=0' 'dataset_name=MNISTBinarized' 'vae_type=AuxilliaryVariationalAutoencoder'
+
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized VAE [2] EXCL=[8] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[8]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=VariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized HVAE [5, 2] EXCL=[8] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[8]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=HierarchicalVariationalAutoencoder'
+# env CUDA_VISIBLE_DEVICES='3' python experiments/main_mnist.py --name "OOD FashionMNISTBinarized AVAE [2] EXCL=[8] IW=10 WU=0 BS=256" with 'n_epochs=1000' 'exclude_labels=[8]' 'warmup_epochs=0' 'dataset_name=FashionMNISTBinarized' 'vae_type=AuxilliaryVariationalAutoencoder'
