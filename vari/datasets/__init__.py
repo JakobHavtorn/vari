34fc06848b86019bc7a8063fca7271d3162f673b
Jakob Havtorn <jdh@corti.ai>
2020-01-18 22:41:58 +0000

    Add SVHN and CIFAR

--- vari/datasets/__init__.py
+++ vari/datasets/__init__.py
@@ -2,3 +2,7 @@ from .spirals import Spirals
 from .moons import Moons
 from .mnist import MNISTReal, MNISTBinarized, MNISTBinarizedLarochelle, FashionMNISTReal, \
                    FashionMNISTBinarized
+from torchvision import datasets
+
+SVHN = datasets.SVHN
+CIFAR10 = datasets.CIFAR10
