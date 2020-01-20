Fix KL order in HVAE

vari/models/vae.py
@@ -218,7 +218,7 @@ class HierarchicalVariationalAutoencoder(nn.Module):
         # Top most latent has unconditional prior and is always required to be given (i.e. copied)
         q_z2, (q_z2_mu, q_z2_sd) = latents[-1]
         if copy_latents[-1]:
-            self.kl_divergences[0] = kld_gaussian_gaussian(q_z2, (q_z2_mu, q_z2_sd))
+            self.kl_divergences[1] = kld_gaussian_gaussian(q_z2, (q_z2_mu, q_z2_sd))
             self.kl_divergence = self.kl_divergences[0]
             p_z1, (p_z1_mu, p_z1_sd) = self.decoder[0](q_z2)
         else:
@@ -226,12 +226,13 @@ class HierarchicalVariationalAutoencoder(nn.Module):
     
         q_z1, (q_z1_mu, q_z1_sd) = latents[-2]
         if copy_latents[-2]:
-            self.kl_divergences[1] = kld_gaussian_gaussian(q_z1, (q_z1_mu, q_z1_sd), (p_z1_mu, p_z1_sd))
+            self.kl_divergences[0] = kld_gaussian_gaussian(q_z1, (q_z1_mu, q_z1_sd), (p_z1_mu, p_z1_sd))
             self.kl_divergence = self.kl_divergence + self.kl_divergences[1]
             x, px_args = self.decoder[1](q_z1)
         else:
-            self.kl_divergences[1] = (kld_gaussian_gaussian(q_z1, (q_z1_mu, q_z1_sd), p_param=(p_z1_mu, p_z1_sd)) +
-                                      kld_gaussian_gaussian(q_z1, (p_z1_mu, p_z1_sd), p_param=(q_z1_mu, q_z1_sd))) / 2
+            self.kl_divergences[0] = kld_gaussian_gaussian(p_z1, (q_z1_mu, q_z1_sd), p_param=(p_z1_mu, p_z1_sd))
+            # self.kl_divergences[0] = (kld_gaussian_gaussian(q_z1, (q_z1_mu, q_z1_sd), p_param=(p_z1_mu, p_z1_sd)) +
+                                    #   kld_gaussian_gaussian(q_z1, (p_z1_mu, p_z1_sd), p_param=(q_z1_mu, q_z1_sd))) / 2
             # self.kl_divergence += self.kl_divergences[1]
             x, px_args = self.decoder[1](p_z1)
 