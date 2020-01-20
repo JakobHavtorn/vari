Fix two bugs with KL

experiments/main_synthetic2d.py
@@ -108,7 +108,7 @@ def run(device, dataset_name, dataset_kwargs, vae_type, n_epochs, batch_size, le
                 # Importance sampling
                 kl_divergence = log_sum_exp(kl_divergence.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  # (B, 1, 1)
                 likelihood = log_sum_exp(likelihood.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  # (B, 1, 1)
-                kl_divergences = [log_sum_exp(kl_divergence.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  for kl_divergence in model.kl_divergences]
+                kl_divergences = [log_sum_exp(kl.view(-1, importance_samples, 1), axis=1, sum_op=torch.mean).view(-1, 1)  for kl in model.kl_divergences]
 
                 total_elbo += elbo.mean().item()
                 total_likelihood += likelihood.mean().item()

vari/models/vae.py
@@ -227,7 +227,7 @@ class HierarchicalVariationalAutoencoder(nn.Module):
         q_z1, (q_z1_mu, q_z1_sd) = latents[-2]
         if copy_latents[-2]:
             self.kl_divergences[1] = kld_gaussian_gaussian(q_z1, (q_z1_mu, q_z1_sd), (p_z1_mu, p_z1_sd))
-            self.kl_divergence += self.kl_divergences[1]
+            self.kl_divergence = self.kl_divergence + self.kl_divergences[1]
             x, px_args = self.decoder[1](q_z1)
         else:
             self.kl_divergences[1] = (kld_gaussian_gaussian(q_z1, (q_z1_mu, q_z1_sd), p_param=(p_z1_mu, p_z1_sd)) +