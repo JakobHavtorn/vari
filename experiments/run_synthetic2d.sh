e3cd6b35e050dc569bdac4864e794586d43d5632
Jakob Havtorn <jdh@corti.ai>
2020-01-30 17:02:02 +0000

    Update

--- experiments/run_synthetic2d.sh
+++ experiments/run_synthetic2d.sh
@@ -7,79 +7,79 @@ sudo cp -r ../vari ../vari-run-synthetic2
 cd ../vari-run-synthetic2
 pwd
 
-# MOONS
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
---name "OOD Moons σ=0.01 VAE z=[2] h=[[16, 16, 8, 8]] IW=100 WU=0 BS=256" with \
-'n_epochs=1000' \
-'dataset_name=Moons' \
-'dataset_kwargs.noise=0.01' \
-'importance_samples=100' \
-'model_kwargs.z_dim=[2]' \
-'model_kwargs.h_dim=[[16, 16, 8, 8]]' \
-# --unobserved
+
 
 env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
---name "OOD Moons σ=0.01 VAE z=[2] h=[[16, 16], [8, 8]] IW=1 WU=0 BS=128" with \
-'n_epochs=1000' \
-'batch_size=128' \
+--name "OOD Moons σ=0.01 VAE FixedVariance=0.01 z=[2] h=[[64, 64, 32, 32]] IW=1 WU=0 BS=10000" with \
+'n_epochs=10000' \
+'batch_size=10000' \
 'dataset_name=Moons' \
 'dataset_kwargs.noise=0.01' \
 'importance_samples=1' \
 'model_kwargs.z_dim=[2]' \
-'model_kwargs.h_dim=[[16, 16], [8, 8]]' \
-# --unobserved
-
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
---name "OOD Moons σ=0.02 VAE z=[2] h=[[64, 64, 32, 32]] IW=10 WU=0 BS=256" with \
-'n_epochs=1000' \
-'dataset_name=Moons' \
-'dataset_kwargs.noise=0.02' \
-'importance_samples=10' \
-'model_kwargs.z_dim=[2]' \
 'model_kwargs.h_dim=[[64, 64, 32, 32]]' \
-# --unobserved
+'model_kwargs.decoder_distribution=["GaussianFixedVarianceLayer"]' \
+# --unobserved \
 
 env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
---name "OOD Moons σ=0.05 VAE z=[2] h=[[128, 128, 64, 64]] IW=100 WU=0 BS=256" with \
-'n_epochs=1000' \
-'dataset_name=Moons' \
-'dataset_kwargs.noise=0.05' \
-'importance_samples=100' \
-'model_kwargs.z_dim=[2]' \
-'model_kwargs.h_dim=[[128, 128, 64, 64]]' \
---unobserved
-
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
---name "OOD Moons σ=0.00 VAE z=[2] h=[[16, 8]] IW=100 WU=0 BS=256" with \
-'n_epochs=1000' \
-'dataset_name=Moons' \
-'dataset_kwargs.noise=0.00' \
-'importance_samples=100' \
-'model_kwargs.z_dim=[2]' \
-'model_kwargs.h_dim=[[16, 8]]' \
-#--unobserved
-
-
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
---name "OOD Moons σ=0.00 VAE z=[2] h=[[32, 16]] IW=100 WU=0 BS=256" with \
-'n_epochs=1000' \
-'dataset_name=Moons' \
-'dataset_kwargs.noise=0.00' \
-'importance_samples=100' \
-'model_kwargs.z_dim=[2]' \
-'model_kwargs.h_dim=[[32, 16]]' \
-#--unobserved
-
-env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
---name "OOD Moons σ=0.01 VAE z=[2, 2] h=[[64, 64], [32, 32]] IW=100 WU=0 BS=256" with \
-'n_epochs=1000' \
+--name "OOD Moons σ=0.01 VAE FixedVariance=0.01 z=[2, 2] h=[[64, 64], [32, 32]] IW=1 WU=0 BS=10000" with \
+'n_epochs=10000' \
+'batch_size=10000' \
 'dataset_name=Moons' \
 'dataset_kwargs.noise=0.01' \
-'importance_samples=100' \
+'importance_samples=1' \
 'model_kwargs.z_dim=[2, 2]' \
 'model_kwargs.h_dim=[[64, 64], [32, 32]]' \
-# --unobserved
+'model_kwargs.decoder_distribution=["GaussianLayer", "GaussianFixedVarianceLayer"]' \
+# --unobserved \
 
+env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
+--name "OOD Moons σ=0.01 VAE FixedVariance=0.01 z=[2] h=[[64, 64, 32, 32]] IW=1 WU=0 BS=256" with \
+'n_epochs=10000' \
+'batch_size=256' \
+'dataset_name=Moons' \
+'dataset_kwargs.noise=0.01' \
+'importance_samples=1' \
+'model_kwargs.z_dim=[2]' \
+'model_kwargs.h_dim=[[64, 64, 32, 32]]' \
+'model_kwargs.decoder_distribution=["GaussianFixedVarianceLayer"]' \
+# --unobserved \
+
+env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
+--name "OOD Moons σ=0.01 VAE FixedVariance=0.01 z=[2, 2] h=[[64, 64], [32, 32]] IW=1 WU=0 BS=256" with \
+'n_epochs=10000' \
+'batch_size=256' \
+'dataset_name=Moons' \
+'dataset_kwargs.noise=0.01' \
+'importance_samples=1' \
+'model_kwargs.z_dim=[2, 2]' \
+'model_kwargs.h_dim=[[64, 64], [32, 32]]' \
+'model_kwargs.decoder_distribution=["GaussianLayer", "GaussianFixedVarianceLayer"]' \
+# --unobserved \
+
+env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
+--name "OOD Moons σ=0.01 VAE FixedVariance=0.01 z=[2] h=[[64, 64, 32, 32]] IW=1 WU=0 BS=256" with \
+'n_epochs=3000' \
+'batch_size=256' \
+'dataset_name=Moons' \
+'dataset_kwargs.noise=0.01' \
+'importance_samples=1' \
+'model_kwargs.z_dim=[2]' \
+'model_kwargs.h_dim=[[64, 64, 32, 32]]' \
+'model_kwargs.decoder_distribution=["GaussianFixedVarianceLayer"]' \
+# --unobserved \
+
+env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
+--name "OOD Moons σ=0.01 VAE FixedVariance=0.01 z=[2, 2] h=[[64, 64], [32, 32]] IW=1 WU=0 BS=256" with \
+'n_epochs=3000' \
+'batch_size=256' \
+'dataset_name=Moons' \
+'dataset_kwargs.noise=0.01' \
+'importance_samples=1' \
+'model_kwargs.z_dim=[2, 2]' \
+'model_kwargs.h_dim=[[64, 64], [32, 32]]' \
+'model_kwargs.decoder_distribution=["GaussianLayer", "GaussianFixedVarianceLayer"]' \
+# --unobserved \
 
 # # SPIRALS
 # env CUDA_VISIBLE_DEVICES='3' python experiments/main_synthetic2d.py \
